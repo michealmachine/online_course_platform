66a3c46ffd55555bb7763b275cd48831a50db36d
Double2and9 <micheal0machine@gmail.com>
2024-10-29 18:56:42 +0800

    创建项目

---
+++ src/test/java/com/double2and9/online_course/OnlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.double2and9.online_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
