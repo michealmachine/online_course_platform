95c33104ae947aa8690b66a6e6fdb799d0dee460
Double2and9 <micheal0machine@gmail.com>
2024-11-08 15:51:39 +0800

    添加注释

--- content_service/src/main/java/com/double2and9/content_service/service/impl/TeachplanServiceImpl.java
+++ content_service/src/main/java/com/double2and9/content_service/service/impl/TeachplanServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
