a123eeebc11b752fad1f73049080243a0cd7b380
Double2and9 <micheal0machine@gmail.com>
2025-02-02 17:50:48 +0800

    添加课程服务重试机制的单元测试，验证服务不可用和成功的场景

---
+++ media/src/main/java/com/double2and9/media/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.double2and9.media.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
