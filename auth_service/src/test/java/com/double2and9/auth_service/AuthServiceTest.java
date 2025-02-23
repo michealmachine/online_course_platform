78c9b0fcf975ff04fb472fc52ffdddad47f3fbf9
Double2and9 <micheal0machine@gmail.com>
2025-02-23 22:31:50 +0800

    更新权限控制器集成测试，清理权限数据并添加新的权限类型验证，禁用不稳定的权限树测试

--- auth_service/src/test/java/com/double2and9/auth_service/AuthServiceTest.java
+++ auth_service/src/test/java/com/double2and9/auth_service/AuthServiceTest.java
@@ -1,7 +1,12 @@
 package com.double2and9.auth_service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AuthServiceTest {
+    @Test
+    void contextLoads() {
+        // This test will pass if the application context loads successfully
+    }
 }
